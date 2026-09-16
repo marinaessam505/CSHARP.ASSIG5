c7996e9c53642f32a1cc84b6271109153492a33b
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-16 03:19:40 +0300

    q2

--- ASSIG5/ASSIG5/Program.cs
+++ ASSIG5/ASSIG5/Program.cs
@@ -6,6 +6,36 @@
     // Declare an enum Genre { Fiction, NonFiction, Science }.
     // Add a Genre property to Book, assign it Genre.Science, and print it.
 
+    //enum Genre
+    //{
+    //    Fiction,
+    //    NonFiction,
+    //    Science
+    //}
+
+    //class Book
+    //{
+    //    public Genre Genre { get; set; }
+    //}
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Book book = new Book();
+
+    //        book.Genre = Genre.Science;
+
+    //        Console.WriteLine(book.Genre);
+    //    }
+    //}
+    #endregion
+
+
+
+    #region Question 2
+    // Using the Genre enum above, print the underlying int value of
+    // Genre.Fiction, Genre.NonFiction, and Genre.Science by casting each to int.
     enum Genre
     {
         Fiction,
@@ -27,10 +57,15 @@
             book.Genre = Genre.Science;
 
             Console.WriteLine(book.Genre);
+
+
+
+            Console.WriteLine((int)Genre.Fiction);
+            Console.WriteLine((int)Genre.NonFiction);
+            Console.WriteLine((int)Genre.Science);
         }
     }
     #endregion
-
 
 
 }
